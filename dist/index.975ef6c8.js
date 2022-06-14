c3219c14e214e04a14bd6f4a10e4ec0177d72686
Alina_Misinkevych <misinkevych.alina@gmail.com>
2022-06-15 01:18:04 +0300

    button

--- dist/index.975ef6c8.js
+++ dist/index.975ef6c8.js
@@ -503,39 +503,66 @@ function hmrAcceptRun(bundle, id) {
 }
 
 },{}],"8lqZg":[function(require,module,exports) {
-// $('#playBtn').on("click",function(){
-//     if($(this).hasClass('play'))
-//      {
-//        $(this).removeClass('play');
-//        $(this).addClass('pause');
-//        audio.play();
-//      }
-//     else
-//      {
-//        $(this).removeClass('pause');
-//        $(this).addClass('play');
-//        audio.pause();
-//      }
-//   });
-//   audio.onended = function() {
-//        $("#playBtn").removeClass('pause');
-//        $("#playBtn").addClass('play');
-//   };
-/*stylint-disable*/ let isPlayed = false;
-let playButton = document.getElementById("playBtn");
-function toggle() {
-    let claasName = playButton.className;
-    console.log(claasName);
-    if (claasName === "pause") {
-        playButton.className = "play";
-        playButton.innerHTML = "Play";
-        isPlayed = true;
+// const video = document.querySelector('video');
+// video.onclick = () => video.paused ? video.play() : video.pause();
+/*stylint-disable*/ // let isPlayed=false;
+// let playButton= document.getElementById('playBtn');
+// function toggle () {
+// let claasName = playButton.className;
+// console.log(claasName);
+// if (claasName==='pause') {
+//     playButton.className = 'play';
+//     playButton.innerHTML = 'Play';
+//     isPlayed = true;
+// } else {
+//     playButton.className='pause';
+//     playButton.innerHTML='Pause';
+//     isPlayed='false'
+// }
+// }
+// Take 1 here: https://codepen.io/koenigsegg1/pen/QNMJvP
+// 3-variant
+// var atlanticlight = document.querySelector("#atlanticlight video"),
+// playpause = document.getElementById("playpause"),
+// lefttoplay = document.getElementById("lefttoplay"),
+// righttoplay = document.getElementById("righttoplay"),
+// lefttopause = document.getElementById("lefttopause"),
+// righttopause = document.getElementById("righttopause");
+// atlanticlight.removeAttribute("controls");
+// playpause.style.display = "block";
+// playpause.addEventListener('click',function(){
+// 	if (atlanticlight.paused) {
+// 		atlanticlight.play();
+// 		playpause.classList.add("playing");
+// 		lefttopause.beginElement();
+// 		righttopause.beginElement();
+// 	} else { 
+// 		atlanticlight.pause();
+// 		lefttoplay.beginElement();
+// 		righttoplay.beginElement();
+// 		playpause.classList.remove("playing");
+// 	}
+// },false);
+// 4-variant
+let container = document.querySelector(".video-container"), video = container.querySelector("video"), controls = container.querySelector(".video-controls");
+video.load();
+video.volume = .2;
+container.addEventListener("click", function(e) {
+    if (video.paused) {
+        if (controls.classList.contains("video-controls--play")) controls.classList.remove("video-controls--play");
+        controls.classList.add("video-controls--pause");
+        video.play();
     } else {
-        playButton.className = "pause";
-        playButton.innerHTML = "Pause";
-        isPlayed = "false";
+        if (controls.classList.contains("video-controls--pause")) controls.classList.remove("video-controls--pause");
+        controls.classList.add("video-controls--play");
+        video.pause();
     }
-}
+});
+video.addEventListener("ended", function(e) {
+    video.currentTime = 0;
+    if (controls.classList.contains("video-controls--pause")) controls.classList.remove("video-controls--pause");
+    controls.classList.add("video-controls--play");
+}, false);
 
 },{}]},["1RB6v","8lqZg"], "8lqZg", "parcelRequired7c6")
 
